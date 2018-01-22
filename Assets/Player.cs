5a52e7b9fd144d7012fd9556c765b270b3e9f734
RGill93 <34092222+RGill93@users.noreply.github.com>
2018-01-22 08:46:54 +0000

    added additional movement functionality, created a bullet, with particle effects and and explosion effect, added enemies to the game created obstacles

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,19 +5,66 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRange = 5f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRange = 5f;
 
-    [Tooltip ("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
 
-	// Use this for initialization
-	void Start ()
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        float XThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffSetThisFrame = XThrow * xSpeed * Time.deltaTime;
-	}
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    /*
+     * Processes the spaceship movement and making sure it doesn't go of the screen
+     */
+    private void ProcessTranslation()
+    {
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffSet = xThrow * speed * Time.deltaTime;
+        float yOffSet = yThrow * speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffSet;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float rawYPos = transform.localPosition.y + yOffSet;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    /*
+     * processes the spaceships rotation(pitch, yaw, roll)
+     */
+    private void ProcessRotation()
+    {
+
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControl;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor; 
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
 }
